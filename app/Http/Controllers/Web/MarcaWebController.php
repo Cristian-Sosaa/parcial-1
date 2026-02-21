6d38d75f4efb4c6a3533dad65e928e53cccde557
Cristian Sosa <ss2211@uls.edu.sv>
2026-02-21 14:45:36 -0600

    Agregar CRUD para la gestión de marcas en la interfaz web

---
+++ app/Http/Controllers/Web/MarcaWebController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use App\Models\Marca;
+use Illuminate\Http\Request;
+
+class MarcaWebController extends Controller
+{
+    public function index()
+    {
+        $marcas = Marca::orderByDesc('id')->get();
+        return view('marcas.index', compact('marcas'));
+    }
+
+    public function create()
+    {
+        return view('marcas.create');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'nombre' => ['required', 'string', 'max:255'],
+            'estado' => ['nullable', 'boolean'],
+        ]);
+        
+        $data['estado'] = $request->has('estado');
+
+        Marca::create($data);
+
+        return redirect()->route('marcas.index')->with('ok', 'Marca creada');
+    }
+
+    public function edit(Marca $marca)
+    {
+        return view('marcas.edit', compact('marca'));
+    }
+
+    public function update(Request $request, Marca $marca)
+    {
+        $data = $request->validate([
+            'nombre' => ['required', 'string', 'max:255'],
+            'estado' => ['nullable', 'boolean'],
+        ]);
+
+        $data['estado'] = $request->has('estado');
+
+        $marca->update($data);
+
+        return redirect()->route('marcas.index')->with('ok', 'Marca actualizada');
+    }
+
+    public function destroy(Marca $marca)
+    {
+        $marca->delete();
+        return redirect()->route('marcas.index')->with('ok', 'Marca eliminada');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Web\MarcaWebController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// CRUD Web - Marcas (Interfaz)
+Route::get('/marcas', [MarcaWebController::class, 'index'])->name('marcas.index');
+Route::get('/marcas/create', [MarcaWebController::class, 'create'])->name('marcas.create');
+Route::post('/marcas', [MarcaWebController::class, 'store'])->name('marcas.store');
+Route::get('/marcas/{marca}/edit', [MarcaWebController::class, 'edit'])->name('marcas.edit');
+Route::put('/marcas/{marca}', [MarcaWebController::class, 'update'])->name('marcas.update');
+Route::delete('/marcas/{marca}', [MarcaWebController::class, 'destroy'])->name('marcas.destroy');
